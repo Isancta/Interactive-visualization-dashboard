7547e836e4b12dfac758cc916b864437fa67079e
isancta <isancta@gmail.com>
2019-10-12 14:11:46 -0400

    lets save this

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,100 +3,97 @@ function buildMetadata(sample) {
 // @TODO: Complete the following function that builds the metadata panel
 
 // Use `d3.json` to fetch the metadata for a sample
-	var url = `/sample/${sample}`;
+	var url = `/metadata/${sample}`;
 	
-	const dataPromise = d3.json(url);
-	console.log("Data Promise:",datapromise);
+	data = d3.json(url).then(function(response){
+	console.log(response);		
+	});
 	
 // Use d3 to select the panel with id of `#sample-metadata`
 	
-	var data = d3.select ("#sample-metadata")
+	var panel = d3.select ("#sample-metadata");
 
 // Use `.html("") to clear any existing metadata ???????
 
+	panel.html("");
+ 
 // Use `Object.entries` to add each key and value pair to the panel
 	
-	sample.forEach(function(SampleValues) {
-	console.log(SampleValues)
-	Object.entries(SampleValues).forEach(function([key, value]) {
-	console.log(key, value);
+	
+	Object.entries(data).forEach(function([key, value]) {
+		
+	console.log(`${key}: ${value}`);
+	panel.append([key, value]); 
+	});
 	
 // Hint: Inside the loop, you will need to use d3 to append new
 // tags for each key-value in the metadata.?????
 
 // BONUS: Build the Gauge Chart
 // buildGauge(data.WFREQ);
-}
+};
 
 function buildCharts(sample) {
 
 // @TODO: Use `d3.json` to fetch the sample data for the plots
-
-	data = d3.json(`${data}`).then(function(response){
-	console.log(response);		
-	});
-
-// @TODO: Build a Bubble Chart using the sample data
+	var url = `/samples/${sample}`;
+	d3.json(url).then(function(data) {
+	console.log(data);
 	
-	var otus_ids = data.otus_ids  
- 
-	otus_ids = otus_ids.slice(0,10);
-	var samples_values = 
-	samples_values = samples_values.slice (0,10);
-	var otus_labels = 
-	otus_labels = otus_labels.slice (0,10);
+	const otus_ids = data.otu_ids;
+	//otus_ids = otus_ids.slice(0,10);
 	
-	var Defaul_trace1 = {
-						x: data.otus_ids,
-						y: data.samples_values,
+	const sample_values = data.sample_values;
+	//sample_values = sample_values.slice(0,10);
+	
+	const otus_labels = data.otu_labels;
+	//otus_labels = otus_labels.slice(0,10);
+	
+	var Defaul_trace1 = [{
+						x: otus_ids,
+						y: sample_values,
 						mode: "markers",
 						type: "scatter",
-						text: data.otus_labels
+						text: otus_labels,
 						marker: {
-								size:data.sample_values,
-								color:data.otus_ids,
-	
+								size:sample_values,
+								color:otus_ids,
 								symbol: "cross"
 								}
-						};
+						}];
+	
 
 	var layout = {
 				title: "Belly Button Biodiversity",
 				xaxis: { title: "Otus_ids" },
 				yaxis: { title: "SampleValues" }
+				};
   
-		Plotly.newPlot("plot", data, layout);
+		Plotly.plot("bubble",Defaul_trace1 , layout);
 
 // @TODO: Build a Pie Chart
 	
-	var otus_ids = 
-	otus_ids = otus_ids.slice(0, 10);
-	var sample_values =
-	sample_values = sample_values.slice(0, 10);
-	var otus_labels = 
-	otus_labels = otus_labels.slice(0, 10);
-		var Defaul_trace2 = {
-				labels: [otus_ids],
-				values: [sample_values],
-				type: 'pie'
-				text:otus_labels
-};
-
-	var data = [trace1];
-
-	var layout = {
-	title: "'Pie' Chart",
-};
-
-	Plotly.newPlot("plot", data, layout);
+	
+	var Defaul_trace2 = [{
+				labels: otus_ids,
+				values: sample_values,
+				type: 'pie',
+				text: otus_labels
+		}];
 
 	
 
-   
+	var layout2 = {
+	title: "Pie Chart"
+		};
 
-    
+	Plotly.plot("pie",Defaul_trace2 , layout2);	
+	
+	});
+}
 
-
+// @TODO: Build a Bubble Chart using the sample data
+	
 // HINT: You will need to use slice() to grab the top 10 sample_values,
 // otu_ids, and labels (10 each).
 
@@ -116,6 +113,7 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
+	console.log(`loading sample ${firstSample}`);
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
@@ -129,4 +127,5 @@ function optionChanged(newSample) {
 }
 
 // Initialize the dashboard
+console.log("started");
 init();
